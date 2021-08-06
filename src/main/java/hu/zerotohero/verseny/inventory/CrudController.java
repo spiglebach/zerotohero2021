07984f5a1b1b4a28c48b9c0d139de0a2fd97358f
Szukács Marcell <spiglebach@gmail.com>
2021-08-06 20:35:52 +0200

    left-out commit

--- src/main/java/hu/zerotohero/verseny/inventory/CrudController.java
+++ src/main/java/hu/zerotohero/verseny/inventory/CrudController.java
@@ -1,10 +1,10 @@
-package hu.zerotohero.verseny.crud;
+package hu.zerotohero.verseny.inventory;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/crud")
+@RequestMapping("/inventory")
 public class CrudController {
 
     //TODO implement endpoints
